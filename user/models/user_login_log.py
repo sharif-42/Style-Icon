b63edfce717063348100540c096a9b50b8fe0221
sharif-42 <sharifulcsehstu@gmail.com>
2021-12-27 00:24:44 +0600

    User login log model created, and data save method created

---
+++ user/models/user_login_log.py
@@ -0,0 +1,53 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
+
+
+class UserLoginLog(models.Model):
+    """
+    The model for keeping log of successful and unsuccessful login attempts
+    """
+    used_user_name = models.CharField(
+        _('username'),
+        max_length=254,
+        help_text=_('The email address used for the login attempt.'),
+    )
+    used_email = models.CharField(
+        _('email'),
+        max_length=254,
+        help_text=_('The email address used for the login attempt.'),
+        blank=True,
+        null=True,
+    )
+    is_successful = models.BooleanField(
+        default=True,
+        help_text=_('True denotes that the login was successful'),
+    )
+    attempted_time = models.DateTimeField(
+        _('time attempted'),
+        blank=True,
+        null=True,
+        default=timezone.now,
+    )
+    ip_address = models.GenericIPAddressField(
+        _('IP address'),
+        help_text=_('The IP from which the login was attempted'),
+        blank=True, null=True,
+    )
+    browser = models.CharField(
+        _('browser'),
+        max_length=30,
+        help_text=_('The browser used to login'),
+        blank=True, null=True,
+    )
+
+    class Meta:
+        # TODO: Define index
+        ordering = ('-id',)
+        verbose_name = 'User Login Log'
+        verbose_name_plural = 'User Login Logs'
+
+    def __str__(self):
+        return f"{self.used_user_name} - {self.is_successful}"
--- user/rest_api/views.py
+++ user/rest_api/views.py
@@ -1,4 +1,5 @@
 from django.utils.translation import gettext_lazy as _
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
 from rest_framework import status, generics
 from rest_framework.permissions import IsAdminUser
@@ -12,13 +13,14 @@ from ..serializers import (
     UserInputSerializer,
     UserOutputSerializer
 )
-from ..services.user_service import UserService
+from ..services import UserService, UserLoginLogService
 
 
 class UserListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = UserInputSerializer  # input serializer
     output_serializer_class = UserOutputSerializer  # output serializer
     service_class = UserService
+
     # permission_classes = [IsAdminUser]
     # TODO: add required permission classes
     # TODO: Add pagination for list api
@@ -28,7 +30,7 @@ class UserListCreateAPIView(generics.ListCreateAPIView):
         serializer = self.output_serializer_class(users, many=True)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    def post(self,  request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = self.service_class().create_user(**serializer.validated_data)
@@ -47,21 +49,31 @@ class UserLoginApiView(TokenObtainPairView):
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        authenticated_user = self.service_class(request=request).user_login(
-            username=request.data['username'],
-            password=request.data['password']
-        )
-        if authenticated_user:
-            custom_response = {
-                'user_info': self.output_user_serializer_class(authenticated_user).data,
-                'access_token': serializer.validated_data['access'],
-                'refresh_token': serializer.validated_data['refresh']
-            }
-            return Response(custom_response, status=status.HTTP_200_OK)
-        else:
+        try:
+            serializer.is_valid(raise_exception=True)
+            authenticated_user = self.service_class(request=request).user_login(
+                username=request.data['username'],
+                password=request.data['password']
+            )
+            if authenticated_user:
+                # Log login attempt
+                user_login_log = UserLoginLogService(request=self.request).log_login_attempt(
+                    username=request.data['username'],
+                    is_successful=True,
+                )
+                custom_response = {
+                    'user_info': self.output_user_serializer_class(authenticated_user).data,
+                    'access_token': serializer.validated_data['access'],
+                    'refresh_token': serializer.validated_data['refresh']
+                }
+                return Response(custom_response, status=status.HTTP_200_OK)
+        except AuthenticationFailed:
+            # Log login attempt
+            user_login_log = UserLoginLogService(request=self.request).log_login_attempt(
+                username=request.data['username'],
+                is_successful=False,
+            )
             custom_response = {
                 "message": _("Unable to authenticate with provided credentials!")
             }
             return Response(custom_response, status=status.HTTP_401_UNAUTHORIZED)
-
